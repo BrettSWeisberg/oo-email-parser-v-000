572f14731bbba6d6ab8e689aaff109fb6a51d714
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-01 20:37:31 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailParser
   end
 
   def parse
-    part_1 = self.emails.split([\s,])
+    part_1 = self.emails.split("[\s,]")
     part _2 = []
     part_1.each do |email|
        if part_2.count("email") == nil
