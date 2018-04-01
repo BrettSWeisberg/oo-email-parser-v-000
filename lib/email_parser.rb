bc7eff170435f582607978a15630860ffc546be1
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-01 20:24:32 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,14 +11,16 @@ class EmailParser
 
   def initialize(emails)
     emails = self.new
-    @emails << emails
+  self.class.emails << self
   end
 
   def parse
     part_1 = self.emails.split(" ")
-    #part _2 = []
-    part_1.map.with_index do |email_a , email_b|
-       email_a != email_b
+    part _2 = []
+    part_1.each do |email|
+       if part_2.count("email") == nil
+         part_2 << email
+       end
     end
   end
 
